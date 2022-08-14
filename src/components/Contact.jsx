c591d859a369f3f9195c8141b9bc4e04ed2490aa
Uche <uchaynwankwo@gmail.com>
2022-08-14 20:37:25 +0100

    testing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,7 @@ const Contact = () => {
         console.log("Uche");
         e.preventDefault();
         setButtonText("Sending...");
-        let response = await fetch("http://localhost:5000/contact", {
+        let response = await fetch("/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json;charset=utf-8",
